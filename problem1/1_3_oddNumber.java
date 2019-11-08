af35c62375507c1d05b254d0f81c5b83b0adf370
Dmytro Havrilov <gavrilow@gmail.com>
2019-11-08 23:22:34 +0200

    Update 1_3_oddNumber.java

--- problem1/1_3_oddNumber.java
+++ problem1/1_3_oddNumber.java
@@ -2,3 +2,15 @@
 * В одномерном массиве целых чисел найти наибольшее нечетное число.
 */
 
+public class OddNumber {
+    public static void main(String args[]) {
+        int[] array = new int[]{33, 35, 100020, -1, 787, 999};
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max & array[i] % 2 != 0) {
+                max = array[i];
+            }
+        }
+        System.out.println("Max Odd Number in Array is: " + max);
+    }
+}
